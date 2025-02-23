41e1bfd1928d123867c8ad2100d3c24ccff46f05
auxghlann <orenoytemail@gmail.com>
2025-02-23 15:30:25 +0800

    refactor param name

--- src/base/finiteAutomata.py
+++ src/base/finiteAutomata.py
@@ -3,12 +3,12 @@ from abc import ABC, abstractmethod
 class BaseFiniteAutomata(ABC):
 
     @abstractmethod
-    def validate_input_str(self, input: str) -> bool:
+    def validate_input_str(self, input_str: str) -> bool:
         pass
 
 
 class BaseFiniteStateMachine(ABC):
 
     @abstractmethod
-    def get_output(self, input: str) -> str:
+    def get_output(self, input_str: str) -> str:
         pass
